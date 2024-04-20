3ac3ee0d50f897a960fc29e16abe559be1305b5b
Hitarth Langaliya <hitarth@Hitarths-MacBook-Pro.local>
2024-04-20 21:04:20 +0530

    feat:new install script

---
+++ config/.zshrc
@@ -0,0 +1,13 @@
+################# DO NOT MODIFY THIS FILE #######################
+####### PLACE YOUR CONFIGS IN ~/.config/shazam/zshrc FOLDER #######
+#################################################################
+
+# This file is created by shazam setup.
+# Your original .zshrc is backed up at ~/.zshrc-backup-%y-%m-%d
+
+
+# Load shazam configurations
+source "$HOME/.config/shazam/.shazam"
+
+# Now source oh-my-zsh.sh so that any plugins added in ~/.config/shazam/zshrc/* files also get loaded
+source $ZSH/oh-my-zsh.sh
--- install.sh
+++ install.sh
@@ -1,62 +1,116 @@
-# Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#!/bin/bash
 
-#Install brew packages
-brew install git
-brew install php
-brew install mysql
-brew install node
-brew install nvm
-brew install composer
-brew install wget
-brew install lsd
-brew install postgresql@13
-brew install --cask google-chrome
-brew install --cask visual-studio-code
-brew install --cask warp
-brew install --cask raycast
-brew install --cask notion
-brew install --cask todoist
-brew install --cask postman
-brew install --cask dbeaver
-brew install --cask spotify
+# Declare all the app packages and fonts required
+declare -a packages=("git" "zsh" "wget" "php" "mysql" "node" "nvm" "composer" "lsd" "postgresql@13")
+declare -a applications=("google-chrome" "visual-studio-code" "warp" "raycast" "notion" "todoist" "postman" "dbeaver" "spotify")
+declare -a fonts=("font-jetbrains-mono" "font-meslo-lg-nerd-font")
 
-brew cleanup
+# Install all packages if not installed
+for package in "${packages[@]}"
+do
+    if command -v $package &> /dev/null; then
+        echo $package" is already installed\n"
+    else
+        echo "Installing "$package"...\n"
+        if brew install $package ; then
+            echo $package" Installed\n"
+        else
+            echo "Could not install packages \n" && exit
+        fi
+    fi
+done
 
-# Install and configure oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install all applications if not installed
+for application in "${applications[@]}"
+do
+    if brew list --cask | grep -q "^$application\$"; then
+        echo "$application is already installed\n"
+    else
+        brew install --cask "$application"
+    fi 
+done
 
-echo "Installing powerlevel10k"
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-
-echo "Installing zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-echo "Installing zsh-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# Check if zshrc file already exists create a backup
-if [ -f ~/.zshrc ]
-then
-	mv ~/.zshrc ~/.zsh-pre-autoinstall
-fi
-
-# Copy zshrc file to root
-echo "Creating aliases"
-cd ./zsh/.aliases ~/
-echo "Done!"
-
-echo "Creating zsh config"
-cp ./zsh/.zshrc ~/
-echo "Done!"
-
-echo "Configure p10k"
-if [ ! -f ~/.p10k.zsh ]
-then
-	p10k configure
+# Install all fonts if not installed
+if brew tap | grep -q "homebrew/cask-fonts"; then
+    echo "homebrew/cask-fonts is already tapped"
 else
-	cp ./zsh/.p10k.zsh ~/
+    brew tap homebrew/cask-fonts
 fi
 
-# Source zshrc
-source ~/.zshrc
+for font in "${fonts[@]}"
+do
+    if brew list --cask | grep -q "^$font\$"; then
+        echo "$font is already installed\n"
+    else
+        brew install --cask "$font"
+    fi 
+done
+
+# Backup original .zshrc file in the ~/.config/shazam/backup directory
+mkdir -p ~/.config/shazam/backup
+if mv -n ~/.zshrc ~/.config/shazam/backup/.$(date +"%Y%m%d")-zshrc-backup; then
+    echo "Backed up the current .zshrc to ~/.config/shazam/backup/.$(date +"%Y%m%d")-zshrc-backup\n"
+fi
+
+echo "The setup will be installed in '~/.config/shazam'\n"
+
+echo "Installing oh-my-zsh\n"
+if [ -d ~/.config/shazam/oh-my-zsh ]; then
+    echo "oh-my-zsh is already installed\n"
+    git -C ~/.config/shazam/oh-my-zsh remote set-url origin https://github.com/ohmyzsh/ohmyzsh.git
+elif [ -d ~/.oh-my-zsh ]; then
+    echo "oh-my-zsh in already installed at '~/.oh-my-zsh'. Moving it to '~/.config/shazam/oh-my-zsh'"
+    export ZSH="$HOME/.config/shazam/oh-my-zsh"
+    mv ~/.oh-my-zsh ~/.config/shazam/oh-my-zsh
+    git -C ~/.config/shazam/oh-my-zsh remote set-url origin https://github.com/ohmyzsh/ohmyzsh.git
+else
+    git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.config/shazam/oh-my-zsh
+fi
+
+cp -f config/.aliases ~/.config/shazam/
+cp -f config/.shazam ~/.config/shazam/
+cp -f config/.zshrc ~/
+
+# this will be used to store .zcompdump zsh completion cache files which normally clutter $HOME
+mkdir -p ~/.cache/zsh/
+
+if [ -f ~/.zcompdump ]; then
+    mv ~/.zcompdump* ~/.cache/zsh/
+fi
+
+if [ -d ~/.config/shazam/oh-my-zsh/plugins/zsh-autosuggestions ]; then
+    cd ~/.config/shazam/oh-my-zsh/plugins/zsh-autosuggestions && git pull
+else
+    git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.config/shazam/oh-my-zsh/plugins/zsh-autosuggestions
+fi
+
+if [ -d ~/.config/shazam/oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+    cd ~/.config/shazam/oh-my-zsh/custom/plugins/zsh-syntax-highlighting && git pull
+else
+    git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/shazam/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi
+
+if [ -d ~/.config/shazam/oh-my-zsh/custom/plugins/zsh-completions ]; then
+    cd ~/.config/shazam/oh-my-zsh/custom/plugins/zsh-completions && git pull
+else
+    git clone --depth=1 https://github.com/zsh-users/zsh-completions ~/.config/shazam/oh-my-zsh/custom/plugins/zsh-completions
+fi
+
+if [ -d ~/.config/shazam/oh-my-zsh/custom/plugins/zsh-history-substring-search ]; then
+    cd ~/.config/shazam/oh-my-zsh/custom/plugins/zsh-history-substring-search && git pull
+else
+    git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search ~/.config/shazam/oh-my-zsh/custom/plugins/zsh-history-substring-search
+fi
+
+if [ -d ~/.config/shazam/oh-my-zsh/custom/themes/powerlevel10k ]; then
+    cd ~/.config/shazam/oh-my-zsh/custom/themes/powerlevel10k && git pull
+else
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.config/shazam/oh-my-zsh/custom/themes/powerlevel10k
+fi
+
+if chsh -s $(which zsh) && /bin/zsh -i -c 'omz update'; then
+    echo "Installation complete, exit terminal and enter a new zsh session\n"
+else
+    echo "Something is wrong"
+fi
+exit
