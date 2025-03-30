4aef4cb9f6a663143c97c64d5f7fccdbd10e5fd6
Hitarth Langaliya <dev.hlangaliya@gmail.com>
2025-03-31 01:02:21 +0530

    feat: improvisations

--- install.sh
+++ install.sh
@@ -705,7 +705,10 @@ fi
 # if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
 #     if [ -d "$HOME/.config/shazam2/dotfiles/oh-my-posh" ]; then
 #         if [[ "$SHELL" == "/bin/zsh" ]]; then
+echo "Installing Oh My Posh"
 eval "$(oh-my-posh init zsh --config ~/.config/shazam2/dotfiles/oh-my-posh/theme.toml)"
+
+exec zsh
 #         else
 #             echo "Warning: Shell is not Zsh. Oh My Posh initialization skipped."
 #         fi
